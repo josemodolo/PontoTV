9dcbf6bd55deb7c9151e39530351ced902fe475a
Jose Modolo <josenetozxc@yahoo.com>
2021-08-26 22:24:14 -0300

    projeto em andamento

--- pontotv/src/Franqueados/Franqueados.js
+++ pontotv/src/Franqueados/Franqueados.js
@@ -1,14 +1,31 @@
 import React from 'react'
 import Header from '../HomePage/Components/header/header';
+import Brasil from '../img/full.png'
+import styled from 'styled-components';
+import ReactDOM from "react-dom";
+import Taiwan from "@svg-maps/taiwan";
+import { SVGMap } from "react-svg-map";
+
+const Map = styled.img`
+  width: 110vh;
+`
+const MapDiv = styled.div`
+border: 2px solid black;
+width: 100vh;
+height: 80vh;
+display: flex;
+align-items: center;
+
+`
 
 function Franqueados () {
-
-  
-
   return(
     <div>
       <Header/>
-      <h1>Sou uma area de franqueados</h1>
+        <MapDiv>
+          <Map src={Brasil}/>
+        </MapDiv>
+        <SVGMap map={Taiwan}/>      
     </div>
   )
 }
