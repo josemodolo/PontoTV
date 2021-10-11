b6ed274f13f9def4e7e40a7ef64c785efdbe7d9d
Jose Modolo <josenetozxc@yahoo.com>
2021-10-11 18:25:21 -0300

    projeto em andamento

--- pontotv/src/HomePage/Components/Description/Description.js
+++ pontotv/src/HomePage/Components/Description/Description.js
@@ -24,13 +24,13 @@ const ImgText = styled.p`
 function Description () {
   return(
     <DivHistory>      
-      <ImgText>A <strong>Ponto TV</strong> surgiu em 2013 visando ser referência no mercado de midia indoor, onde a publicidade é oferecida<br/>
-      a qualquer empresa que queira anunciar seus produtos e serviços, através de videos que são exibidos em TV's,<br/>
-      instaladas dentro de estabelecimentos comerciais com grande concentração de pessoas.<br/><br/>
-      Inicialmente atuando em Curitiba (PR) e região metropolitana, hoje contamos com mais de 190 pontos de TV's<br/>
-      pelo Brasil, distribuidos em 10 estados, ao longo de 35 cidades.<br/><br/>
-      Dentro desta proporção, nossa rede impacta 80 mil pessoas diariamente com as publicidades exibidas nas TV's,<br/>
-      que por sua vez, são estrategicamente instaladas em restaurantes, academias, cartórios,<br/>
+      <ImgText>A <strong>Ponto TV</strong> surgiu em <strong>2013</strong> visando ser <strong>referência</strong> no mercado de <strong>midia indoor</strong>, onde a <strong>publicidade</strong> é oferecida<br/>
+      a qualquer <strong>empresa</strong> que queira <strong>anunciar</strong> seus <strong>produtos</strong> e <strong>serviços</strong>, através de <strong>videos</strong> que são <strong>exibidos</strong> em <strong>TV's</strong>,<br/>
+      <strong>instaladas</strong> dentro de <strong>estabelecimentos comerciais</strong> com <strong>grande concentração</strong> de <strong>pessoas</strong>.<br/><br/>
+      <strong>Inicialmente</strong> atuando em <strong>Curitiba</strong> (PR) e região metropolitana, <strong>hoje</strong> contamos com <strong>mais</strong> de <strong>190 pontos</strong> de <strong>TV's</strong><br/>
+      pelo <strong>Brasil</strong>, distribuidos em <strong>10 estados</strong>, ao longo de <strong>35 cidades</strong>.<br/><br/>
+      Dentro desta <strong>proporção</strong>, nossa rede <strong>impacta 80 mil</strong> pessoas <strong>diariamente</strong> com as <strong>publicidades exibidas</strong> nas TV's,<br/>
+      que por sua vez, são <strong>estrategicamente instaladas</strong> em restaurantes, academias, cartórios,<br/>
       lotéricas, hospitais, farmácias, dentre outros.</ImgText>
     </DivHistory>
   )
